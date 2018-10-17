91c6b393ebb8895255196a3152ed0755dd6c8426
Robert J Nagel <rjnagel64@gmail.com>
2018-10-17 09:18:16 -0500

    Add build script and ignore some files

---
+++ build.sh
@@ -0,0 +1,22 @@
+
+
+
+CFLAGS=""
+
+build() {
+	clang $CFLAGS -o reduce RSST/anc/reduce.c
+	clang $CFLAGS -o discharge RSST/anc/discharge.c
+}
+
+clean() {
+	rm -f reduce
+	rm -f discharge
+}
+
+if [ $# = 0 ]; then
+	build
+else
+	"$@"
+fi
+
+# vim: set ft=bash:
