553c82ec8ca214fe5f583cf0d27ccc3256ffc221
Robert J Nagel <rjnagel64@gmail.com>
2018-10-19 11:33:25 -0500

    Notes and TODOs

--- build.sh
+++ build.sh
@@ -4,10 +4,24 @@
 # TODO: Fix whitespace/indentation
 # (three-space indent? mixed tabs/spaces? seems legit.)
 
-# Running time of reduce.c:
+# Running time of './reduce RSST/anc/unavoidable.conf':
 # real	4m51.547s
 # user  4m24.004s
 # sys	0m2.470s
+
+# TODO: running './reduce RSST/anc/U_2822.conf' fails after 3m58.366s with
+# 'Abort trap: 6'. According to the internet, this is probably something to do
+# with an out-of-bound array index. Joy.
+
+# TODO: Separate the files from Robertson and Steinberger.
+#	present{7,8,9,10,11} are Robertson, as is unavoidable.conf
+#	p{5,6,7,8,9,10,11}_2822 are Steinberger, as is U_2822.conf
+#	(Steinberger uses a set that only has D-reducible configurations)
+#	where do L_42 and rules come from?
+#		L_42 is steinberger, I think. (42 rules for discharging)
+#		rules: roberson (32 rules for discharging)
+
+# TODO: For reduce.c, print out configuration number when checking.
 
 CFLAGS="-Wall -Wextra -Wundef -Wold-style-definition -Wstrict-prototypes -Wpedantic --std=c99"
 
@@ -16,15 +30,22 @@ build() {
 	clang $CFLAGS -o discharge RSST/anc/discharge.c
 }
 
+debug-build() {
+	CFLAGS="-g ${CFLAGS}" build
+}
+
 clean() {
 	rm -f reduce
+	rm -rf reduce.dSYM
 	rm -f discharge
+	rm -rf discharge.dSYM
 }
+
+# TODO: Write a function that builds and runs everything, so that the entire project
+# can be run with one command.
 
 if [ $# = 0 ]; then
 	build
 else
 	"$@"
 fi
-
-# vim: set ft=bash:
