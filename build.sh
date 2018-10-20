a447834f0f7f3a2c70688ae3e7707141840b6de1
Robert J Nagel <rjnagel64@gmail.com>
2018-10-19 21:05:00 -0500

    Add some commands to run everything

--- build.sh
+++ build.sh
@@ -3,31 +3,20 @@
 
 # TODO: Fix whitespace/indentation
 # (three-space indent? mixed tabs/spaces? seems legit.)
+#	* Done for reduce.c
 
 # TODO: Use structures instead of arrays, so things have names.
 
-# Running time of './reduce RSST/anc/unavoidable.conf':
-# real	4m51.547s
-# user  4m24.004s
-# sys	0m2.470s
 
-# TODO: running './reduce RSST/anc/U_2822.conf' fails after 3m58.366s with
+# TODO: running './reduce steinberger/U_2822.conf' fails after 3m58.366s with
 # 'Abort trap: 6'. According to the internet, this is probably something to do
 # with an out-of-bound array index. Joy.
-
-# TODO: Separate the files from Robertson and Steinberger.
-#	present{7,8,9,10,11} are Robertson, as is unavoidable.conf
-#	p{5,6,7,8,9,10,11}_2822 are Steinberger, as is U_2822.conf
-#	(Steinberger uses a set that only has D-reducible configurations)
-#	where do L_42 and rules come from?
-#		L_42 is steinberger, I think. (42 rules for discharging)
-#		rules: roberson (32 rules for discharging)
 
 CFLAGS="-Wall -Wextra -Wundef -Wold-style-definition -Wstrict-prototypes -Wpedantic --std=c99"
 
 build() {
-	clang $CFLAGS -o reduce RSST/anc/reduce.c
-	clang $CFLAGS -o discharge RSST/anc/discharge.c
+	clang $CFLAGS -o reduce src/reduce.c
+	clang $CFLAGS -o discharge src/discharge.c
 }
 
 debug-build() {
@@ -39,6 +28,39 @@ clean() {
 	rm -rf reduce.dSYM
 	rm -f discharge
 	rm -rf discharge.dSYM
+}
+
+check-robertson() {
+	# Run everything for Robertson's four color program.
+
+	# Check reducibility of the unavoidable set.
+	./reduce robertson/unavoidable.conf
+
+	# ./discharge robertson/present7 robertson/unavoidable.conf robertson/rules 0 1
+	# ./discharge robertson/present8 robertson/unavoidable.conf robertson/rules 0 1
+	# ./discharge robertson/present9 robertson/unavoidable.conf robertson/rules 0 1
+	# ./discharge robertson/present10 robertson/unavoidable.conf robertson/rules 0 1
+	# ./discharge robertson/present11 robertson/unavoidable.conf robertson/rules 1 1
+}
+
+check-steinberger() {
+	# Run everything for Steinberger's four color program.
+
+	# Check reducibility of the unavoidable set.
+	./reduce steinberger/U_2822.conf
+
+	# ./discharge steinberger/p5_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p6_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p7_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p8_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p9_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p10_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+	# ./discharge steinberger/p11_2822 steinberger/U_2822.conf steinberger/L_42 0 1
+}
+
+check-both() {
+	check-robertson
+	check-steinberger
 }
 
 # TODO: Write a function that builds and runs everything, so that the entire project
