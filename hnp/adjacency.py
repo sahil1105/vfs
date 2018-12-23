484d98e5b654a7d67945e2450f3d8bb9332be1a7
Bertram Ludaescher <ludaesch@illinois.edu>
2018-12-22 18:23:09 -0600

    vanilla version w/o numpy

--- hnp/adjacency.py
+++ hnp/adjacency.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python 
 import sys
-import numpy as np
-
 epsilon = 1e-6  # low precision input (1e-9 misses edges)
 
-points = np.loadtxt(sys.argv[1], delimiter=',', usecols=(1,2))
+points = [] 
+
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        ID, x, y = line.rstrip("\n").split(",")
+        points.append( (float(x), float(y)) )
 
 N = len(points)
 
 edges = []
 for i in range(N):
     for j in range(i+1,N):
-        dist = sum( (points[i] - points[j])**2 )
+        dist = ( points[i][0] - points[j][0] )**2 + ( points[i][1] - points[j][1] )**2
         if abs(dist - 1.0) < epsilon:
             edges.append( (i+1,j+1) )
-            
+
 print(edges)
 print("Found", len(edges), "edges of unit length.")
